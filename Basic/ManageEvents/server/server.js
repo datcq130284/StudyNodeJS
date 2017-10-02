aab40d8494fac953e62c45955f4e177b045b7ead
Chau Quoc Dat <dchau5@csc.com>
2017-10-02 17:03:22 +0700

    Update

---
+++ Basic/ManageEvents/server/server.js
@@ -0,0 +1,18 @@
+console.log("Handling Web Server Events");
+
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(req,res){
+    console.log("Request received", req.headers);
+    res.end("Thanks, I got your request");
+});
+
+server.on('upgrade',function(req,socket,head){
+    console.log("Upgrade the connection to a web socket connection");
+});
+
+var port = 3456;
+server.listen(port, function(){
+    console.log("HTTP Server Listening");
+});
